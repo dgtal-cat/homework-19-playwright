5983b354167c80cde9cae698899dca3d067b795d
Aleksey Kremnev <dgtal.cat@gmail.com>
2022-10-11 07:27:21 +0300

    оптимизация и чистка

--- tests/js-ru-tests.spec.ts
+++ tests/js-ru-tests.spec.ts
@@ -1,14 +1,17 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Функцилнальные тесты сайта learn.javascript.ru', () => {
-    test('Открытие статьи "Циклы while и for" с главной страницы', async ({ page }) => {
+
+    test.beforeEach(async ({ page }) => {
         await page.goto('https://learn.javascript.ru/')
+    })
+
+    test('Открытие статьи "Циклы while и for" с главной страницы', async ({ page }) => {
         await page.locator('a:has-text("Циклы while и for")').click()
         await expect(page.locator('.main__header-title')).toHaveText('Циклы while и for')
     })
 
     test('Работа строки поиска', async ({ page }) => {
-        await page.goto('https://learn.javascript.ru/')
         await page.locator('.text-input__frontpage-search__input').click()
         await page.locator('.text-input__frontpage-search__input').fill('switch')
         await page.locator('.text-input__frontpage-search__input').press('Enter')
@@ -16,7 +19,6 @@ test.describe('Функцилнальные тесты сайта learn.javascri
     })
 
     test('Смена темы сайта на темную и обратно', async ({ page }) => {
-        await page.goto('https://learn.javascript.ru/')
         await expect(page.locator('.theme-changer__icon >> nth=1')).toHaveClass('theme-changer__icon theme-changer__icon_dark-theme')
         await page.locator('.theme-changer__icon >> nth=1').click()
         await expect(page.locator('.theme-changer__icon >> nth=0')).toHaveClass('theme-changer__icon theme-changer__icon_light-theme')
@@ -27,13 +29,11 @@ test.describe('Функцилнальные тесты сайта learn.javascri
     })
 
     test('Переход на форум из главного меню навигации', async ({ page }) => {
-        await page.goto('https://learn.javascript.ru/')
         await page.locator('a:has-text("Форум")').click()
         await expect(page.url()).toEqual('https://javascript.ru/forum/')
     })
 
-    test.only('Переключение языка сайта', async ({ page }) => {
-        await page.goto('https://learn.javascript.ru/')
+    test('Переключение языка сайта', async ({ page }) => {
         await page.locator('.sitetoolbar__dropdown-button >> nth=0').click()
         await page.locator('.supported-langs__title:has-text("English")').click()
         await expect(page.locator('h1.frontpage-banner__title')).toHaveText('The Modern JavaScript Tutorial')
