dfac1550388d4e34d99e4f3c13361859788743ba
Aleksey Kremnev <dgtal.cat@gmail.com>
2022-10-30 10:44:45 +0300

    исправил ошибку в слове

--- tests/js-ru-tests.spec.ts
+++ tests/js-ru-tests.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe('Функцилнальные тесты сайта learn.javascript.ru', () => {
+test.describe('Функциональные тесты сайта learn.javascript.ru', () => {
 
     test.beforeEach(async ({ page }) => {
         await page.goto('https://learn.javascript.ru/')
